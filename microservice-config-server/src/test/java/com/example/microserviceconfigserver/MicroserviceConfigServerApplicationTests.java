36475db7ba4fbd56911e51420e0d5d3e176e93d1
luyi <1023239574@qq.com>
2019-12-23 15:54:56 +0800

    add

---
+++ microservice-config-server/src/test/java/com/example/microserviceconfigserver/MicroserviceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microserviceconfigserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
